782e83785c0ccc97b9ea623fec360c8c2ba058a6
OshadaSenaweera <oshada.senaweera@gmail.com>
2025-12-11 02:13:19 +1000

    update code

--- sptm only model.R
+++ sptm only model.R
@@ -266,6 +266,67 @@ pit_vals_sto5 <- cpo_obj_sto5$pit
 # Histogram of PIT values: should be ~Uniform(0,1)
 hist(pit_vals_sto5, breaks = 20, main = "PIT histogram", xlab = "PIT values")
 
+################################################################################
+
+# model
+sto_6 <- y ~ 1 +
+  f(area_id2, model = "besag", graph = g, group = time_id, 
+    control.group = list(model = "rw1"))
+
+m_sto6 <- inla(
+  sto_6,
+  family = "poisson",
+  data = crime_weekly,
+  control.predictor = list(compute = TRUE),
+  control.compute = list(dic = TRUE, waic = TRUE, cpo = TRUE, config = TRUE),
+  # safer numerical settings:
+  control.inla = list(
+    strategy = "simplified.laplace"       # gaussian, simplified.laplace (default) or laplace
+    #int.strategy = "eb",                   # ccd (default), grid or eb (empirical bayes)
+    #h = 0.005,                            #smaller step size for derivatives
+    #tolerance = 1e-4                      # stricter convergence criterion
+  ),
+  control.mode = list(restart = TRUE),
+  verbose = TRUE
+)
+summary(m_sto6)
+
+crime_weekly$pred_meansto6 <- m_sto6$summary.fitted.values$mean
+
+ggplot(crime_weekly, aes(x = y, y = pred_meansto6)) +
+  geom_point(alpha = 0.3, color = "steelblue") +
+  geom_abline(intercept = 0, slope = 1, color = "red", linetype = "dashed") +
+  labs(
+    x = "Actual weekly crime count",
+    y = "Predicted mean (INLA)",
+    title = "Actual vs Predicted Crime Counts"
+  ) +
+  theme_minimal()
+
+rmse_value <- rmse(crime_weekly$y, crime_weekly$pred_meansto6)
+mae_value  <- mae(crime_weekly$y, crime_weekly$pred_meansto6)
+r2_value   <- cor(crime_weekly$y, crime_weekly$pred_meansto6)^2
+
+c(RMSE = rmse_value, MAE = mae_value, R2 = r2_value)
+
+cpo_obj_sto6 <- m_sto6$cpo
+pit_vals_sto6 <- cpo_obj_sto6$pit
+
+# Histogram of PIT values: should be ~Uniform(0,1)
+hist(pit_vals_sto6, breaks = 20, main = "PIT histogram", xlab = "PIT values")
+
+###############################################################################
+
+
+
+
+
+
+
+
+
+
+
 
 
 
